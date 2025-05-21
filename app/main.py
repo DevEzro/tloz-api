85f900ee798bb556f06be7ae58fab4b242263e93
DevEzro <davidtestout@outlook.com>
2025-05-21 14:07:05 +0200

    changes

--- app/main.py
+++ app/main.py
@@ -1,23 +1,7 @@
-from fastapi import FastAPI, Depends
-from sqlmodel import Session
-
-from app.database import get_session
-from app.models import Character
-from app.schemas import CharacterRead
-from app.crud import get_characters
-
-from sqlmodel import SQLModel
-from app.database import engine
+from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/greeting")
+@app.get("/")
 async def greeting():
-    return {"message": "🤗 Hi! Hope you enjoy this TLOZ® API to play with! If you like this repository, please consider giving it a star ⭐!"}
-
-@app.get("/characters", response_model=list[CharacterRead])
-def read_characters(session: Session = Depends(get_session)):
-    characters = get_characters(session)
-    print("Characters found:", characters)
-    return characters
-
+    return {"message": """🤗 Hi! Hope you enjoy this TLOZ® API to play with! If you like this repository, please consider giving it a star ⭐!"""}

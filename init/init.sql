a35c4e33e88ef7a2e5c892e253eaac7e58a84f36
DevEzro <davidtestout@outlook.com>
2025-05-29 13:07:19 +0200

    restruycuture initq1l

--- init/init.sql
+++ init/init.sql
@@ -31,7 +31,7 @@ CREATE TABLE IF NOT EXISTS objects (
     id SERIAL PRIMARY KEY,
     name VARCHAR(100),
     damage INT,
-    age INT,
+    age VARCHAR(100),
     location VARCHAR(100),
     condition VARCHAR(100)
 );
